0e85d6bb54bc2e4b6525b1bb684d3d2614fd7e23
Jignesh Kalal <jigneshkalal.436@gmail.com>
2022-12-22 16:43:05 +0000

    css

--- src/UI-Compoments/Accordion.jsx
+++ src/UI-Compoments/Accordion.jsx
@@ -1,22 +1,28 @@
-import React, { useState } from 'react';
-import Detailes from './Detailes';
+import { useState } from "react";
+import Detailes from "./Detailes";
 
-const Accordion = ( { content: {id,first,last,dob,gender,description,picture,country} }) => {
+const Accordion = ({
+  content: { id, first, last, dob, gender, description, picture, country },
+}) => {
   const [isActive, setIsActive] = useState(false);
 
   return (
     <div className="accordion-item">
       <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
-              
-              <div><img src={picture} alt='' />{`${first} ${last}`}</div>
-              
-            
-              <div>{isActive ?
-                  <i className="fa fa-angle-down " aria-hidden="true"></i>
-                  : <i className="fa fa-angle-up " aria-hidden="true"></i>}
-              </div>
+        <div id='profile'>
+          <img src={picture} alt="" />
+          {`${first} ${last}`}
+        </div>
+
+        <div>
+          {isActive ? (
+            <i className="fa fa-chevron-down" aria-hidden="true"></i>
+          ) : (
+            <i className="fa fa-chevron-up" aria-hidden="true"></i>
+          )}
+        </div>
       </div>
-      {isActive && <Detailes {...{dob, gender, description,country}} />}
+      {isActive && <Detailes {...{ dob, gender, description, country }} />}
     </div>
   );
 };
